2f7b52490d0565195b7a01ce949c88198b112317
Sriram <sri761177@gmail.com>
2023-04-12 12:00:37 +0530

    Add page folder

---
+++ src/pages/messages/Messages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Messages.scss"
+const Messages = () => {
+  return (
+    <div className='Messages'>Messages</div>
+  )
+}
+
+export default Messages
---
+++ src/pages/orders/Order.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Order.scss"
+
+const Order = () => {
+  return (
+    <div className='Order'>Order</div>
+  )
+}
+
+export default Order
