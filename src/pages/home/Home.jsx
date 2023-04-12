da6903cb65e5c4f4625cdda34c75a0be196edaa6
Sriram <sri761177@gmail.com>
2023-04-12 22:21:09 +0530

    Add Carousel & temp data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./Home.scss"
+import {cards} from "../../data"
 import Featured from '../../components/featured/Featured'
+import TrustedBy from '../../components/trustedBy/TrustedBy'
 
 const Home = () => {
   return (
-    <div className='home'><Featured/></div>
+    <div className='home'> 
+      <Featured/>
+      <TrustedBy/>
+      <Slide>
+        {cards.map(card=>(
+          <CatCard key={card.id} item={card}/>
+        ))}
+      </Slide>
+    </div>
   )
 }
 
